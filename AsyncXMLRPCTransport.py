521fabef970ceebe9b22c5023140431a2c9ef4f8
Mark Guagenti <mark.guagenti@synapse-wireless.com>
2008-10-16 17:07:56 +0000

    Incremental checkin of keep-alive async xml-rpc

--- AsyncXMLRPCTransport.py
+++ AsyncXMLRPCTransport.py
@@ -47,6 +47,156 @@ class HttpLibFakeSocket(object):
     return self._data
 
 
+class Keep_Alive_Session(asyncore.dispatcher):
+  def __init__(self, host, request_body, transport):
+    asyncore.dispatcher.__init__(self)
+    self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.host = host
+    self.getparser = transport.getparser
+    self.waiting_on_response = False
+    self.rx_buf = ""
+    self.tx_buf = ""
+
+    # Connect to host and POST request
+    self.connect(urllib.splitnport(host, 80))
+
+  def send_request(self, request_body):
+    if self.waiting_on_response:
+      #raise Exception("BAD!!!")
+      pass
+
+    keep_alive = "\r\n"
+    if not self.connected:
+      keep_alive = "Connection: Keep-Alive\r\n\r\n"
+    self.tx_buf += "POST /RPC2 HTTP/1.1\r\n" \
+        "Content-Type: text/xml\r\n" \
+        "User-Agent: AsyncXMLRPC/Python\r\n" \
+        "Host: %s\r\n" \
+        "Content-length: %d\r\n" \
+        "%s%s" % (self.host, len(request_body), keep_alive, request_body)
+    self.deferred = Deferred.Deferred()
+    return self.deferred
+
+  def handle_connect(self):
+    #Try to start sending once connected
+    self.handle_write()
+
+  def handle_read(self):
+    recv_data = self.recv(4096)
+    if recv_data:
+      #log.debug("Received data: %s" % rcvData)
+      self.rx_buf += recv_data
+      if self.rx_buf.find("</methodResponse>") > -1:
+        self.waiting_on_response = False
+        resp = httplib.HTTPResponse(HttpLibFakeSocket(cStringIO.StringIO(self.rx_buf)))
+        resp.begin()
+        if resp.status == httplib.OK:
+          if resp.chunked:
+            #Skip the chunk lengths
+            self.dispatchXmlResponse(''.join(resp.fp.readlines()[1:-2]))
+          else:
+            self.dispatchXmlResponse(resp.fp.read())
+          #assert resp.will_close
+        else:
+          self.deferred.runErrback(xmlrpclib.ProtocolError(self.host, str(resp.status), resp.reason, {}))
+        self.rx_buf = ""
+        #self.close()
+
+  def writable(self):
+    # Ready to send
+    return self.tx_buf
+
+  def handle_write(self):
+    self.waiting_on_response = True
+    numSent = self.send(self.tx_buf)
+    #if __debug__ and numSent:
+      #log.debug("Sent data: %s" % self.txBuf[:numSent])
+    self.tx_buf = self.tx_buf[numSent:]
+
+  def handle_close(self):
+    #This seems to be only called when the other side closes the connection
+    self.close()
+    self.connected = False
+    self.deferred.runErrback(xmlrpclib.ProtocolError(self.host, '500', 'Socket Closed', {}))
+
+  def handle_error(self):
+    t, v, nil = sys.exc_info()
+    self.deferred.runErrback(
+      xmlrpclib.ProtocolError(self.host, '500',
+                              "Socket Error (%s:%s)" % (t, v) , {})
+    )
+    self.close()
+
+  def handle_expt(self):
+    #From EffBot:
+    #Called when a connection fails (Windows), or when out-of-band data arrives (Unix)
+
+    #Since we aren't doing OOB
+    err = self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+    if err:
+      msg = os.strerror(err)
+      if msg == 'Unknown error':
+        msg += ' - '
+        msg += str(err)
+      self.deferred.runErrback(socket.error((err, msg)))
+      log.error(msg) #Using error instead of exception since there is not a stack trace
+      self.close()
+
+  def dispatchXmlResponse(self, response):
+      p, u = self.getparser()
+      try:
+        #log.debug("Dispatching response")
+        p.feed(response)
+        p.close()
+        result = u.close()
+
+        # Post this result to the Deferred object bound to this response
+        self.deferred.runCallback(result[0])
+
+      # Catch XMLRPC "faultCode" response parsing (raised by the 'close' method)
+      except (xmlrpclib.Fault, xml.parsers.expat.ExpatError), e:
+        self.deferred.runErrback(e)
+
+
+class Keep_Alive_Transport(xmlrpclib.Transport):
+  """Asynchronously handles an HTTP transaction to an XML-RPC server.
+     Designed to be passed to the xmlrpclib.ServerProxy ctor.
+
+     Standard XMLRPC HTTP Transport uses the TCP session context to
+     correlate command/response.  Therefore, a new session must be created
+     for each method call, and is closed when the response is complete.
+     This class is a factory of TCP sessions to handle these method calls.
+
+     A ServerProxy instance will create a single instance of this Transport
+     implementation.  Each method call results in a 'request' call.  We create
+     a new MethodSession for each request.
+
+     Note:
+     The asynchronous nature of this library means that separate method
+     invocations could get executed OUT OF ORDER.  The method call data is 
+     posted to the sockets library in call-order, but each method gets its own socket.
+     Socket polling will send on all active sockets in parallel, so methods could 
+     complete in any order.
+
+     If you need guaranteed order of method invocation, the client should use XMLRPC MultiCall,
+     or provide event-based interlocks.  An asynchronous compatible MultiCall adaptation
+     based on xmlrpclib.MultiCall is provided in this module (see below).
+  """
+  def __init__(self, *args, **kwargs):
+    xmlrpclib.Transport.__init__(self, *args, **kwargs)
+    self.hosts = {}
+
+  def request(self, host, handler, request_body, verbose=0):
+    """Send a complete request, and parse the response.
+       Return a Deferred response object.       
+    """
+    if self.hosts.has_key(host) and self.hosts[host].connected:
+      pass
+    else:
+      self.hosts[host] = Keep_Alive_Session(host, request_body, self)
+    return (self.hosts[host].send_request(request_body),)
+
+
 class AsyncXMLRPCTransport(xmlrpclib.Transport):
   """Asynchronously handles an HTTP transaction to an XML-RPC server.
       Designed to be passed to the xmlrpclib.ServerProxy ctor.
@@ -78,6 +228,7 @@ class AsyncXMLRPCTransport(xmlrpclib.Transport):
         Return a Deferred response object.       
     """
     return (AsyncXMLRPCTransport.MethodSession(host, request_body, self).deferred,)
+
 
 
   class MethodSession(asyncore.dispatcher):
@@ -205,3 +356,19 @@ class MultiCall(xmlrpclib.MultiCall):
     """
     mcIter = xmlrpclib.MultiCall.__call__(self)
     return mcIter.results
+
+if __name__ == '__main__':
+  import logging
+  logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(msecs)03d %(levelname)-8s %(name)-8s %(message)s', datefmt='%H:%M:%S')
+
+  srv_async = xmlrpclib.ServerProxy("http://192.168.1.66:8080", Keep_Alive_Transport())
+  #srv_async = xmlrpclib.ServerProxy("http://localhost:8080", Keep_Alive_Transport())
+
+  def on_result(result):
+    print result
+    srv_async.gatewayVersion()
+
+  srv_async.gatewayVersion().addCallbacks(on_result)
+
+  while True:
+    asyncore.poll(0.001)
