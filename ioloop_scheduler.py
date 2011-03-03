80b59c344e4dcb787248d6c3cec22175d037c318
Mark Guagenti <mark.guagenti@synapse-wireless.com>
2011-03-03 14:09:29 +0000

    Added support for using the tornado ioloop as a scheduler

---
+++ ioloop_scheduler.py
@@ -0,0 +1,59 @@
+"""Emulates the apy EventScheduler API for use with tornado.ioloop"""
+
+
+import time
+
+import tornado.ioloop
+
+
+class IOLoopEventElement(object):
+    def __init__(self, func, delay=0.0, *args, **kwargs):
+        self.func = func
+        self.orig_func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.delay = delay
+        self._running = True
+
+    def run(self):
+        value = self.func(*self.args, **self.kwargs)
+        self._running = False
+        if isinstance(value, bool):
+            tornado.ioloop.IOLoop.instance().add_timeout(time.time() + self.delay, self.run)
+            self._running = True
+        elif isinstance(value, float) or isinstance(value, int):
+            self.delay = value
+            tornado.ioloop.IOLoop.instance().add_timeout(time.time() + self.delay, self.run)
+            self._running = True
+
+    def start(self):
+        tornado.ioloop.IOLoop.instance().add_timeout(time.time() + self.delay, self.run)
+        self._running = True
+        self.func = self.orig_func
+
+    def Stop(self, *args, **kwargs):
+        self.func = self.Stop
+        self._running = False
+
+    stop = Stop
+
+
+def schedule(delay, callable, *args, **kwargs):
+    """Emulates the EventScheduler.schedule API"""
+    event = IOLoopEventElement(callable, delay, *args, **kwargs)
+    tornado.ioloop.IOLoop.instance().add_timeout(time.time() + delay, event.run)
+    return event
+
+class IOLoopScheduler(object):
+    def schedule(self, delay, callable, *args, **kwargs):
+        """Emulates the EventScheduler.schedule API"""
+        event = IOLoopEventElement(callable, delay, *args, **kwargs)
+        tornado.ioloop.IOLoop.instance().add_timeout(time.time() + delay, event.run)
+        return event
+
+    @classmethod
+    def instance(cls):
+        """Returns a global IOLoopScheduler instance."""
+        if not hasattr(cls, "_instance"):
+            cls._instance = cls()
+        return cls._instance
